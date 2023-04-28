ed29160012f0420487bf7ef85fc389043aef0a1d
Moises Rodriguez <moisesdavid96@hotmail.com>
2023-04-28 18:10:33 -0400

    small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
-import { types } from "util";
 
 const inter = Inter({ subsets: ["latin"] });
 
